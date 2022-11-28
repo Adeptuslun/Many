cec0bb640a75264e4d62a728ccdc0aa6e1b1554b
Andronion <adeptuslun@gmail.com>
2022-11-28 18:00:05 +0500

    ввод массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите длинну массива") ;
+int n = Convert.ToInt32(Console.ReadLine());
+
+  String[] array = new string[n];
+  for (int i = 0; i < n; i++)
+  {
+    Console.WriteLine("Введите значение") ;
+    string count=Console.ReadLine();
+    array[i] = count;
+
+    Console.Write($"{array[i]} ");
+  }
