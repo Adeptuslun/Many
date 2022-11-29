e6e7b368734d8531cdbe6cbba6563fa54d8e157f
Andronion <adeptuslun@gmail.com>
2022-11-29 17:59:54 +0500

    last commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите длинну массива") ;
+﻿Console.WriteLine("Введите длинну массива") ; 
 int n = Convert.ToInt32(Console.ReadLine());
 
   String[] array = new string[n];
@@ -8,16 +8,30 @@ int n = Convert.ToInt32(Console.ReadLine());
   {
     Console.WriteLine($"Введите значение");
     string? count=Convert.ToString(Console.ReadLine());
-    array[i] = count;
+    array[i] = count; 
 if (count.Length <=3)
     {
           j++;
     }
   }
-   String[] Arry = new string[j];
+  Console.WriteLine($"Заданный массив:");
+  for (int i = 0; i < n; i++)
+  {
+      Console.Write($" {array[i]} /");
+
+  }
+    String[] Arry = new string[j];
+    Console.WriteLine();
+   
+    Console.WriteLine($"Искомый массив:");
+if (j==0)
+   {
+    Console.Write($"не может быть создан");
+   }
 for (int i = 0; i < j; i++)
 {
     if (array[i].Length<=3)
     {Arry[i] = array[i];
-    Console.Write($"{Arry[i]}/");}
+    Console.Write($" {Arry[i]}/ ");
+    }
 }
