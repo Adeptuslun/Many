d7da70f6e4ca860e5bac2517530840b3980bae9f
Andronion <adeptuslun@gmail.com>
2022-11-29 16:57:19 +0500

    no_list

--- Program.cs
+++ Program.cs
@@ -2,37 +2,21 @@
 int n = Convert.ToInt32(Console.ReadLine());
 
   String[] array = new string[n];
-  List<string> Limit = new List<string>();
+  String[] Arry = new string[n];
+  int j = 0;
   for (int i = 0; i < n; i++)
   {
     Console.WriteLine($"Введите значение");
-    string count=Console.ReadLine();
+    string? count=Convert.ToString(Console.ReadLine());
     array[i] = count;
 if (count.Length <=3)
     {
-      Limit.Add(count);
+          j++;
     }
-    
   }
-  Console.WriteLine();
-  String[] Arry = new string[Limit.Count];
-  for (int i = 0; i < Arry.Length; i++)
-  {
-    Arry[i] = Limit[i];
+for (int i = 0; i < j; i++)
+{
+    if (array[i].Length<=3)
+    Arry[i] = array[i];
     Console.Write($"{Arry[i]}/");
-  }
-
-  Console.WriteLine();
-/*List<string> Limit = new List<string>();
-for (int i = 0; i < n; i++)
-  {
-   
-    string count = array[i];
-    if (count.Length >=3)
-    {
-      Limit = new List<string>() {count};
-    }
-
-    Console.Write($"{array[i]} ");
-  }
-  Console.WriteLine();*/
+}
